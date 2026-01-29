3c44339ac210b92a826740718309a7a2b3c57b47
Marino Ricardo <74591655+marinoricardo@users.noreply.github.com>
2026-01-29 15:51:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -776,9 +776,18 @@ app.post('/generate-pdf', async (req, res) => {
   const data = req.body;
 
   try {
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
+    // const browser = await puppeteer.launch({
+    //   args: ['--no-sandbox', '--disable-setuid-sandbox']
+    // });
+
+     const browser = await puppeteer.launch({
+  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox'
+  ]
+});
+
     const page = await browser.newPage();
 
     const html = generatePremiumCVHtml(data);
